8f894703a30b0aa3c3983fcbc27077530cc105bd
Manuel Eberl <manuel@pruvisto.org>
2022-11-19 02:28:44 +0100

    more instance information

--- main/instance.py
+++ main/instance.py
@@ -1,4 +1,5 @@
 from django.db import connection
+import pycountry
 
 logos = {
     'aardwolf': 'aardwolf.png', 'bonfire': 'bonfire.png', 'bookwyrm': 'bookwyrm.png', 'calckey': 'calckey.png', 'castopod': 'castopod.svg',
@@ -26,7 +27,7 @@ def mk_bool(x):
     return None
 
 class Instance:
-    def __init__(self, host, local_domain, software, software_version, registrations_open, users, active_month, active_halfyear, local_posts, last_update, uptime, dead, up):
+    def __init__(self, host, local_domain, software, software_version, registrations_open, users, active_month, active_halfyear, local_posts, last_update, uptime, country_code, dead, up):
         host = host.lower()
         self.host = host
         if local_domain is None:
@@ -45,8 +46,10 @@ class Instance:
         self.local_posts = mk_int(local_posts)
         self.dead = dead
         self.up = up
+        self.last_update = last_update
         self.uptime = None
         if uptime is not None:
+            uptime = uptime / 100
             try:
                 if uptime == 1.0:
                     self.uptime = '100 %'
@@ -63,24 +66,24 @@ class Instance:
         self.icon = logos.get(self.software)
         self.webfinger_url = f'https://{self.local_domain}/.well-known/webfinger'
         
+        if country_code is not None:
+            self.country = pycountry.countries.get(alpha_2 = country_code)
+        
         stats = None
         if self.users is not None:
             stats = f'users: {self.users}'
             tmp = list()
-            # uptime is broken in instances.social
-#            for x, y in [('active_month', 'last month'), ('active_halfyear', 'last 6 months'), ('uptime', 'uptime')]:
-            for x, y in [('active_month', 'last month'), ('active_halfyear', 'last 6 months')]:
+            for x, y in [('active_month', 'last month'), ('active_halfyear', 'last 6 months'), ('uptime', 'uptime')]:
                 z = getattr(self, x)
                 if z is None: continue
                 tmp.append(f'{y}: {z}')
             if tmp: stats = stats + ' (' + '; '.join(tmp) + ')'
         self.stats = stats
             
-        try:
-            self.last_update = datetime.datetime.fromisoformat(last_update)
+        if self.last_update is not None:
             self.last_update_pretty = self.last_update.strftime('%-d %B %Y, %H:%M')
-        except:
-            self.last_update = None
+        else:
+            self.last_update_pretty = None
             
     def compare_key(self, us):
         if self.software == 'mastodon':
@@ -105,15 +108,15 @@ class Instance:
         return self.host == other.host
 
 def naked_instance(name):
-    return Instance(name.lower(), None, None, None, None, None, None, None, None, None, None, None, None)
+    return Instance(name.lower(), None, None, None, None, None, None, None, None, None, None, None, None, None)
 
 def get_instance(name):
     try:
         with connection.cursor() as cur:
-            cur.execute('SELECT name, local_domain, software, software_version, registrations_open, users, active_month, active_halfyear, local_posts, last_update, uptime, dead, up FROM instances WHERE name=%s LIMIT 1', [name.lower()])
+            cur.execute('SELECT name, local_domain, software, software_version, registrations_open, users, active_month, active_halfyear, local_posts, last_update, uptime, country_code, dead, up FROM instances WHERE name=%s LIMIT 1', [name.lower()])
             row = cur.fetchone()
         if row is None: return naked_instance(name)
-        i = Instance(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12])
+        i = Instance(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13])
         return i
     except Exception as e:
         print(e)
